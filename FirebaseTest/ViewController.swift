f353dcf2c33c00ce480d6ab9a6f3a816f11cb217
Yamamotonasu <deep.blue.deep@outlook.jp>
2020-01-09 22:57:14 +0900

    first commit

---
+++ FirebaseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTest
+//
+//  Created by 山本裕太 on 2020/01/09.
+//  Copyright © 2020 山本裕太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
