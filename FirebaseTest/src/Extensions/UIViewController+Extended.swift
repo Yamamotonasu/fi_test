9adba197980249341aa9c27daf4bc50f35810825
Yamamotonasu <deep.blue.deep@outlook.jp>
2020-01-11 10:49:16 +0900

    add library and LoginView

---
+++ FirebaseTest/src/Extensions/UIViewController+Extended.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Extended.swift
+//  FirebaseTest
+//
+//  Created by 山本裕太 on 2020/01/11.
+//  Copyright © 2020 山本裕太. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import NSObject_Rx
+
+extension UIViewController {
+    
+}
--- FirebaseTest/src/Views/LoginViewController.swift
+++ FirebaseTest/src/Views/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  FirebaseTest
 //
 //  Created by 山本裕太 on 2020/01/09.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+/**
+ * ログイン画面
+ */
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
